81bcd34939af7807f07d73549b376e9b2fa3b5ea
sergiocardenasg <sergiocardenas@utexas.edu>
2021-01-13 22:40:04 -0600

    email added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_043550) do
+ActiveRecord::Schema.define(version: 2021_01_14_043852) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_043550) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "playlist_songs", "playlists"
